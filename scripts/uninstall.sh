da69369eb436e5b78a096d88b7e73aab6b38a8a1
Alif <arfshl@protonmail.com>
2025-08-06 13:28:59 +0700

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -10,7 +10,8 @@ apt remove x11-repo tur-repo -y
 
 rm $PREFIX/etc/proot-distro/portadesx.sh
 rm /data/data/com.termux/files/usr/bin/portadesx-cli
-rm /data/data/com.termux/files/usr/bin/portadesx-x11
+rm /data/data/com.termux/files/usr/bin/portadesx-gui
+rm /data/data/com.termux/files/usr/bin/portadesx-help
 
 sed -i '/LD_PRELOAD=/system/lib64/libskcodec.so/d' $PREFIX/etc/bash.bashrc
 sed -i '/pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"/d' $PREFIX/etc/bash.bashrc
