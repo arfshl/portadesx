76d176394efdf443974a9e0fb77474f6040872cc
Alif <arfshl@protonmail.com>
2025-08-06 14:07:21 +0700

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,7 +4,7 @@ proot-distro remove portadesx
 
 proot-distro clear-cache
 
-apt remove proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y && apt autoremove
+apt remove proot-distro termux-x11 pulseaudio vulkan-loader-android mesa-zink virglrenderer-mesa-zink virglrenderer-android -y && apt autoremove -y
 
 apt remove x11-repo tur-repo -y
 
