548ceefa17119b4f730aec7a8edfc96710c52a22
Alif <arfshl@protonmail.com>
2025-08-08 15:01:15 +0700

    Update and rename remove-paralells.sh to uninstall-paralells.sh

--- scripts/uninstall-paralells.sh
+++ scripts/uninstall-paralells.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 proot-distro remove portadesx
+
 rm $PREFIX/etc/proot-distro/portadesx.sh
 rm /data/data/com.termux/files/usr/bin/portadesx-cli
 rm /data/data/com.termux/files/usr/bin/portadesx-gui
