80dfbfd27b392942e7921bf0e35ae172c3c50401
Alif <arfshl@protonmail.com>
2025-08-13 21:14:17 +0700

    Update portadesx.sh

--- scripts/portadesx.sh
+++ scripts/portadesx.sh
@@ -1,10 +1,10 @@
 DISTRO_NAME="PortadesX"
-TARBALL_URL['aarch64']="https://github.com/portadesx/portadesx/releases/download//portadesx-aarch64.tar.xz"
-TARBALL_URL['arm']="https://github.com/portadesx/portadesx/releases/download//portadesx-armhf.tar.xz"
-TARBALL_URL['x86_64']="https://github.com/portadesx/portadesx/releases/download//portadesx-x86_64.tar.xz"
-TARBALL_SHA256['aarch64']=""
-TARBALL_SHA256['arm']=""
-TARBALL_SHA256['x86_64']=""
+TARBALL_URL['aarch64']="https://github.com/portadesx/portadesx/releases/download/noble-202508131329/portadesx-aarch64.tar.xz"
+TARBALL_URL['arm']="https://github.com/portadesx/portadesx/releases/download/noble-202508131329/portadesx-armhf.tar.xz"
+TARBALL_URL['x86_64']="https://github.com/portadesx/portadesx/releases/download/noble-202508131329/portadesx-x86_64.tar.xz"
+TARBALL_SHA256['aarch64']="3fd604c674777214fe8599719e917184beb5e3dd607a6f72235a96c811858eec"
+TARBALL_SHA256['arm']="5aa69ddee7f5619f8d157d376b6479c8a4689006013cb0ceee5641f05b5640dd"
+TARBALL_SHA256['x86_64']="0a4c7203f363e7d968eb556fd6bca5c452e13f61964f4dad141a511113a9ca68"
 distro_setup() {
         run_proot_cmd ln -s /storage/emulated/0/ /home/portadesx/Desktop/android_files
 }
