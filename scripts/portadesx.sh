8c78d4abcc5089131b22e9a8fc2963badc9a85be
Alif <arfshl@protonmail.com>
2025-08-22 14:31:22 +0700

    Update portadesx.sh

--- scripts/portadesx.sh
+++ scripts/portadesx.sh
@@ -1,6 +1,6 @@
 DISTRO_NAME="PortadesX"
 TARBALL_URL['aarch64']="https://github.com/portadesx/portadesx/releases/download/noble-aarch64/portadesx-aarch64.tar.xz"
-TARBALL_SHA256['aarch64']="8bc627e1f6e4db837106992fbefb425514c059df7fef2343d0ad489459a68fb4"
+TARBALL_SHA256['aarch64']="cda326aa26cf06e864f09ff11cd9a971a070208273490eb4e31509963f10e37f"
 distro_setup() {
         run_proot_cmd ln -s /storage/emulated/0/ /home/portadesx/Desktop/android_files
 }
