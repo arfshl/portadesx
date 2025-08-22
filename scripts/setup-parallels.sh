9f7de49978be3d5a905618aada008434f59e9d94
Alif <arfshl@protonmail.com>
2025-08-22 13:57:46 +0700

    Update setup-parallels.sh

--- scripts/setup-parallels.sh
+++ scripts/setup-parallels.sh
@@ -16,7 +16,7 @@ proot-distro login portadesx --shared-tmp -- /bin/sh -c 'kill -9 $(pgrep -f "x11
 virgl_test_server_android &
 termux-x11 :0 >/dev/null &
 sleep 3
-proot-distro login portadesx --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - portadesx -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe startxfce4"'
+proot-distro login portadesx --shared-tmp -- /bin/sh -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - portadesx -c "DISPLAY=:0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session startxfce4"'
 EOF
 
 # Make all of them executable
